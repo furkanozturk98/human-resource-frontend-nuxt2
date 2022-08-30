255e4ed77f1b884b8622b63f95e07fd57ee2ff5e
Furkan <furkan.ozturk.98@hotmail.com>
2022-08-30 20:22:16 +0300

    refactor: update navbar links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,10 +8,20 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Employees</a>
+            <NuxtLink
+              class="nav-link"
+              to="/employees"
+            >
+              Employees
+            </NuxtLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Companies</a>
+            <NuxtLink
+              class="nav-link"
+              to="/companies"
+            >
+              Companies
+            </NuxtLink>
           </li>
         </ul>
 
